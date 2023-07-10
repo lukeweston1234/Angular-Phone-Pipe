950d77d1038994513240041d340ecd82452409f4
lukeweston1234 <45077571+lukeweston1234@users.noreply.github.com>
2023-07-10 16:40:25 -0400

    Init

---
+++ phone-number.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({ name: "phoneNumber", standalone: true })
+export class PhoneNumberPipe implements PipeTransform {
+  transform(rawNum: string) {
+    const numWithOnlyNums = rawNum.replace(/\D/g, "");
+    if (numWithOnlyNums.length < 10 || numWithOnlyNums.length > 11) return rawNum; //Just return if format is different, feel free to extend this or return something like NA
+    let offset = numWithOnlyNums.length == 10 ? 0 : 1;
+    return `(${numWithOnlyNums.slice(0 + offset, 3 + offset)})-${numWithOnlyNums.slice(
+      3 + offset,
+      6 + offset
+    )}-${numWithOnlyNums.slice(6 + offset)}`;
+  }
+}
